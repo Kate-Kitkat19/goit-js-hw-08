495e12b0427deae14cf50ad9d72bf081f15dab3e
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-23 20:40:50 +0300

    player - get the time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_NAME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(getTime, 1000));
+
+function getTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      console.log(seconds);
+      localStorage.setItem(KEY_NAME, JSON.stringify(seconds));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+localStorage.removeItem('KEY_NAME');
