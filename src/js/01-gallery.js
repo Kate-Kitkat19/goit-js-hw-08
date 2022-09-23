cdbfe74579de2b0b137bcc2ac115f15c447f6bd8
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-23 17:44:57 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+const galleryRef = document.querySelector('div.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+  )
+  .join('');
+console.log('galleryMarkup', galleryMarkup);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
