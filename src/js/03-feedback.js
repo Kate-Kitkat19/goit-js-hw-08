a3d875df7573ad22557a7fd091fe186956e5c584
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-09-23 21:29:41 +0300

    player done, 1/2 the form task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const formData = {};
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', throttle(onFormChange, 500));
+
+function onFormChange(evt) {
+  const key = evt.target.name;
+  const value = evt.target.value;
+  formData[key] = value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
 
